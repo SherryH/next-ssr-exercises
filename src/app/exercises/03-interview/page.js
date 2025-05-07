0cc91ae44beb90df18469a1b505f0ea1a0348c2c
SherryH <enjoy.wind7rain@gmail.com>
2025-05-07 19:47:42 +0800

    Completed exercise 3

--- src/app/exercises/03-interview/page.js
+++ src/app/exercises/03-interview/page.js
@@ -1,35 +1,35 @@
-'use client';
 import React from 'react';
-import { useMediaQuery } from 'react-responsive';
 
 import Interview from './Interview';
 import './styles.css';
+import Side from './Side';
+
+// Hydration mismatch error happens because Client and Server can be different when loaded
+// here this happens if on Client isDesktop is true, then the <Interview/> will not be loaded like on Server
+
+// My approach: initially, isDesktop is always false
+// then when mounted, compute isDesktop
+// => Actually, it was because the Server component was rendered inside a Client component!!!
+// => Make this page.js a Server Component to include the Server interview.js works!
+// => NO, actually it does not work..
+// => use CSS to control mediaQuery to avoid the issue!!
 
 function InterviewExercise() {
-  const isDesktop = useMediaQuery({
-    query: '(min-width: 500px)',
-  });
+  // Oh no I am breaking the Rule of Hooks! if setting let isDesktop = false;
+  // const [isDesktop, setIsDesktop] = React.useState(false);
+
+  // React.useEffect(() => {
+  //   //ERROR: We can't use a hook inisde useEffect!!!
+  //   const isNewDesktop = useMediaQuery({
+  //     query: '(min-width: 500px)',
+  //   });
+  //   setIsDesktop(isNewDesktop);
+  // }, []);
 
   return (
     <main>
       <Interview />
-      {isDesktop && (
-        <aside>
-          <img
-            src="/gwen-artist.png"
-            alt="Portrait of the artist"
-          />
-          <h2>About the Artist</h2>
-          <p>
-            Gwen Altaria is a contemporary artist known for
-            her unique blend of traditional oil painting
-            techniques and pop culture references. Born and
-            raised in London, England, Vivi had an early
-            love for both the fine arts and the fantastical
-            worlds of video games.
-          </p>
-        </aside>
-      )}
+      <Side />
     </main>
   );
 }
